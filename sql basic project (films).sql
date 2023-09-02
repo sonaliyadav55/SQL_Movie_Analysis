5c8ca0205752b5afdb792941ce2d5d60331633b0
sonali yadav <143174661+sonaliyadav55@users.noreply.github.com>
2023-09-02 15:05:29 +0530

    Update sql basic project (films).sql

--- sql basic project (films).sql
+++ sql basic project (films).sql
@@ -1,29 +1,34 @@
 #PROJECT
 use mavenmovies
-#1
+#1. Write an SQL query to count the number of characters except for the spaces for each actor. Return the first 10 actors' name lengths along with their names.
 
-#select * from actor
+
 
 select concat(first_name,last_name) as actors_name, 
 length(concat(first_name,last_name))
 from actor a limit 10;
 
 
-#2
-#use mavenmovies
+#2. List all Oscar awardees(Actors who received the Oscar award) with their full names and the length of their names.
+
 
 select concat(first_name,last_name) as full_name,length(concat(first_name,last_name)) ,awards from actor_award
 where awards like'%oscar%';
 
-#3
+
+  
+#3. Find the actors who have acted in the film ‘Frost Head.’
+  
 
 select concat(first_name,' ',last_name),title from actor a 
 inner join film_actor fc on a.actor_id = fc.actor_id
 inner join film f on fc.film_id = f.film_id
 where title = 'Frost Head';
 
-#4
-#select* from actor
+
+#4. Pull all the films acted by the actor ‘Will Wilson.’
+
+  #select* from actor
 #select * from film
 
 select concat(first_name,' ',last_name) as actor_name,title from film f
@@ -31,7 +36,9 @@ inner join film_actor fc on f.film_id = fc.film_id
 inner join actor a on fc.actor_id = a.actor_id
 where concat(first_name,' ',last_name) =  'Will Wilson';
 
-#5
+
+#5. Pull all the films which were rented and return them in the month of May.
+
 #select * from film
 #select * from inventory
 #select * from rental
@@ -42,11 +49,17 @@ inner join rental r on inv.inventory_id = r.inventory_id
 where return_date like '____-05-__%';
 
 
-#6
+#6. Pull all the films with ‘Comedy’ category.
+
 #select * from category
 #select * from film
 
 select * from film f
 left join film_category fc on  f.film_id = fc.film_id
 inner join category c on fc.category_id = c.category_id
+
+
+
+
+  
 where name = 'comedy';
